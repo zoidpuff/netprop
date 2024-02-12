6633a99f0c786f08160e532216f62e41accdf506
zoidpuff <zoidpuff@gmail.com>
2024-02-12 18:46:53 +0100

    blurg

--- Code/runNetProp.R
+++ Code/runNetProp.R
@@ -67,7 +67,7 @@ for(BINARIZE in c(TRUE)) {
         print(paste0("Started binarize: ", BINARIZE, " NormFunc: ", NORMFUNC[[3]] ))
 
         # Replace the outer for loop with a foreach loop
-        aurocs <- foreach(trait = unique(assocDataFilt10$diseaseId), .combine = 'list', .packages = 'dplyr') %dopar% {
+        aurocs <- foreach(trait = unique(assocDataFilt10$diseaseId), .combine = 'rbind', .packages = 'dplyr') %dopar% {
 
             assocDataFiltTemp <- assocDataFilt10 %>% filter(diseaseId == trait)
 
@@ -90,7 +90,7 @@ for(BINARIZE in c(TRUE)) {
         print(paste0("Finished binarize: ", BINARIZE, " NormFunc: ", NORMFUNC[[3]] ))
         binned <- ifelse(BINARIZE, "_binarized_", "_weighted_")
 
-        do.call(rbind,aurocs) %>% as.data.frame() %>% write.csv(paste0(netpropPath,"/results/overallPhenos_aurocResults",binned,NORMFUNC[[3]],".csv"))
+        write.csv(aurocs,paste0(netpropPath,"/results/overallPhenos_aurocResults",binned,NORMFUNC[[3]],".csv"))
     }
 }
 
