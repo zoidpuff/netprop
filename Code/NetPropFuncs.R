1fd664dce0d8201ca7c8e8c21b375dbb343776e7
zoidpuff <zoidpuff@gmail.com>
2024-02-12 17:28:33 +0100

    asd

--- Code/NetPropFuncs.R
+++ Code/NetPropFuncs.R
@@ -51,7 +51,8 @@ return(list(seedVecMaster,seedGeneInds,seedGeneScores))
 
 avgAUROC <- function(network, seedList, nRep, recoverSizeVec, binarize = TRUE,NormFunc = NULL,settingsForNormFunc) {
 
-    no_cores <- detectCores()
+    no_cores <- min(detectCores(),10)
+
     cl <- makeCluster(no_cores)
     registerDoParallel(cl)
 
